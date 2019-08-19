46cc1a8f395002eabb7ce59430c6fb26f35eabc4
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-08-19 20:21:11 +0300

    added solution for pow task

--- src/main/java/core/basesyntax/CustomMathPow.java
+++ src/main/java/core/basesyntax/CustomMathPow.java
@@ -3,12 +3,19 @@ package core.basesyntax;
 public class CustomMathPow {
 
     /**
-     * Написать метод pow(int number, int exponential), который является аналогом метода
-     * Math.pow() и принимает 2 целых числа:
-     *      number - число которое надо возводить в какую-то степень
-     *      exponential - значение степени
+     * Написать метод pow(int number, int exponential), который является аналогом метода Math.pow()
+     * и принимает 2 целых числа: number - число которое надо возводить в какую-то степень
+     * exponential - значение степени
      */
     public int pow(int number, int exponential) {
-        return 0;
+        if (exponential == 0) {
+            return 1;
+        }
+        int result = number;
+        for (int i = 1; i < exponential; i++) {
+            result *= number;
+        }
+        return result;
+
     }
 }
