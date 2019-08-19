e7844dc94d2ba44210bf65e6b75fc1d34b6cb33f
Sasha192 <sasha192.bunin@gmail.com>
2019-08-19 17:19:18 +0300

    Oleksandr Bunin Commit

--- src/main/java/core/basesyntax/CustomMathPow.java
+++ src/main/java/core/basesyntax/CustomMathPow.java
@@ -8,7 +8,22 @@ public class CustomMathPow {
      *      number - число которое надо возводить в какую-то степень
      *      exponential - значение степени
      */
-    public int pow(int number, int exponential) {
-        return 0;
+    public long pow(long number, long exponential) {
+
+        if ( number < 0 || exponential < 0) throw new IllegalArgumentException("Number and Exponential must be positive number");
+    	if ( exponential == 0 ) return 1;
+    	if ( number == 0 ) return 0;
+
+    	long retValue = 1;
+    	int i = 0;
+    	while(exponential > 0){
+    		if( (exponential&1) == 1 ){
+    			retValue = retValue*number;
+    		}
+    		exponential = exponential >> 1;
+    		number = number*number;
+    	}
+
+    	return retValue;
     }
 }
