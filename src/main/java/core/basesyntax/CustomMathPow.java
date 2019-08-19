2bc0204f7e190a053f93024ff3ed998d045caaf4
RostykRosolovskyi <38504193+RostykRosolovskyi@users.noreply.github.com>
2019-08-19 20:15:17 +0300

    realize method

--- src/main/java/core/basesyntax/CustomMathPow.java
+++ src/main/java/core/basesyntax/CustomMathPow.java
@@ -1,14 +1,36 @@
 package core.basesyntax;
 
-public class CustomMathPow {
+class CustomMathPow {
 
     /**
      * Написать метод pow(int number, int exponential), который является аналогом метода
      * Math.pow() и принимает 2 целых числа:
-     *      number - число которое надо возводить в какую-то степень
-     *      exponential - значение степени
+     * number - число которое надо возводить в какую-то степень
+     * exponential - значение степени
      */
     public int pow(int number, int exponential) {
-        return 0;
+        if (exponential < 0) {
+            number = 1 / number;
+            exponential = -exponential;
+        }
+        if(exponential==0)
+            return 1;
+        int y=1;
+        while (exponential>1)
+        {
+            if(exponential%2==0)
+            {
+                number*=number;
+                exponential=exponential/2;
+            }
+            else
+            {
+                y=number*y;
+                number*=number;
+                exponential=(exponential-1)/2;
+
+            }
+        }
+        return number * y;
     }
 }
