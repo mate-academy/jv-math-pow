8629ca1c059ec12664802cc8c1c1d04416baa7c1
Taras Khalak <tarasulo@gmail.com>
2019-08-19 21:09:42 +0300

    'Solution'

--- src/main/java/core/basesyntax/CustomMathPow.java
+++ src/main/java/core/basesyntax/CustomMathPow.java
@@ -9,6 +9,12 @@ public class CustomMathPow {
      *      exponential - значение степени
      */
     public int pow(int number, int exponential) {
-        return 0;
+        if (exponential == 0) {
+            return 1;
+        }
+        for (int i = 1; i < exponential; i++) {
+            number *= number;
+        }
+        return number;
     }
 }
