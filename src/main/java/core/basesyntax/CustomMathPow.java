1c1e5a05253745db27855d801671308809e8aea0
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-20 01:19:50 +0300

    Update CustomMathPow.java

--- src/main/java/core/basesyntax/CustomMathPow.java
+++ src/main/java/core/basesyntax/CustomMathPow.java
@@ -9,7 +9,7 @@ public class CustomMathPow {
      *      exponential - значение степени
      */
     public int pow(int number, int exponential) {
-        if(exponential < 1) return 1;
+        if(exponential < 1 || exponential == 0) return 1;
         return number*pow(number, exponential-1);
     }
 }
