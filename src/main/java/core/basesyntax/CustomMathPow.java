3c195694c80dce544e1e01b7ebaf12d6ba90c264
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-19 22:25:20 +0300

    pow method finished in CustomMathPow class

--- src/main/java/core/basesyntax/CustomMathPow.java
+++ src/main/java/core/basesyntax/CustomMathPow.java
@@ -1,14 +1,22 @@
 package core.basesyntax;
 
 public class CustomMathPow {
+    private int maxInt = pow(2, 32);
 
     /**
      * Написать метод pow(int number, int exponential), который является аналогом метода
      * Math.pow() и принимает 2 целых числа:
-     *      number - число которое надо возводить в какую-то степень
-     *      exponential - значение степени
+     * number - число которое надо возводить в какую-то степень
+     * exponential - значение степени
      */
     public int pow(int number, int exponential) {
-        return 0;
+        long result = 1;
+        for (int i = 0; i < exponential; i++) {
+            result *= number;
+            if (result >= maxInt) {
+                return maxInt;
+            }
+        }
+        return (int) result;
     }
 }
