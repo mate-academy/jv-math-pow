35eb5a33970492639153b3c69a0a3ab8b056f51a
Kokhanevich <nike.k1995@gmail.com>
2019-08-19 20:36:39 +0300

    added solution for HW1-pow (part 2)

--- src/main/java/core/basesyntax/CustomMathPow.java
+++ src/main/java/core/basesyntax/CustomMathPow.java
@@ -9,10 +9,10 @@ public class CustomMathPow {
      *      exponential - значение степени
      */
     public int pow(int number, int exponential) {
-        /* int countNumber= 1;
-        for (int i=1; i<=exponential; i++) {
-            countNumber = countNumber*number;
-        } */
-        return (int)  StrictMath.pow(number,exponential);
+        int countNumber = 1;
+        for (int i = 1; i <= exponential; i++) {
+            countNumber = countNumber * number;
+        }
+        return countNumber;
     }
 }
