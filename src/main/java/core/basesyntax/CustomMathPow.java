b85eca2186119e6cf49a40b10ef6bd6fed192a6b
kuhaj <alex.kukhtin.dev@gmail.com>
2019-08-20 08:16:24 +0300

    Correct form math pow function

--- src/main/java/core/basesyntax/CustomMathPow.java
+++ src/main/java/core/basesyntax/CustomMathPow.java
@@ -9,6 +9,13 @@ public class CustomMathPow {
      *      exponential - значение степени
      */
     public int pow(int number, int exponential) {
-        return 0;
+        if (exponential == 0) {
+            return 1;
+        }
+        int result = number;
+        for (int i = 1; i < exponential; i++) {
+            result *= number;
+        }
+        return result;
     }
 }
