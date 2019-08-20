f7fd7666859715c65cec6bbe24d92cdce0c7c43f
Саша Бунин <sasha192.bunin@gmail.com>
2019-08-20 09:46:26 +0300

    Update CustomMathPow.java

--- src/main/java/core/basesyntax/CustomMathPow.java
+++ src/main/java/core/basesyntax/CustomMathPow.java
@@ -1,30 +1,33 @@
 package core.basesyntax;
 
-@SuppressWarnings("ALL")
+
 public class CustomMathPow {
     /**
      * Написать метод pow(int number, int exponential), который является аналогом метода
      * Math.pow() и принимает 2 целых числа:
-     *      number - число которое надо возводить в какую-то степень
-     *      exponential - значение степени
+     * number - число которое надо возводить в какую-то степень
+     * exponential - значение степени
      */
     public int pow(int number, int exponential) {
-        if ( number < 0 || exponential < 0) {
+        if (number < 0 || exponential < 0) {
             throw new IllegalArgumentException("Number and Exponential must be positive number");
+        } else if (exponential == 0 || number == 0) {
+            if (exponential == 0) {
+                return 1;
+            } else {
+                return number;
+            }
         }
-    	if ( exponential == 0 ) return 1;
-    	if ( number == 0 ) return 0;
-
-		int retValue = 1;
-    	while(exponential > 0){
-    		if( (exponential&1) == 1 ){
-    			retValue = retValue*number;
-    		}
-    		exponential = exponential >> 1;
-			number = number*number;
-			//System.out.println(retValue);
-    	}
-		System.out.println(retValue);
-    	return retValue;
+        int retValue = 1;
+        while (exponential > 0) {
+            if ((exponential & 1) == 1) {
+                retValue = retValue * number;
+            }
+            exponential = exponential >> 1;
+            number = number * number;
+            //System.out.println(retValue);
+        }
+        System.out.println(retValue);
+        return retValue;
     }
 }
