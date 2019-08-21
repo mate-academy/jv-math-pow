9a6ac59b0f5b64719bfa8b9b5ba4ac7beae9bc39
Koval <xui>
2019-08-21 15:33:18 +0300

    hw1.3 MathPow

--- src/main/java/core/basesyntax/CustomMathPow.java
+++ src/main/java/core/basesyntax/CustomMathPow.java
@@ -9,14 +9,17 @@ public class CustomMathPow {
      *      exponential - значение степени
      */
     public int pow(int number, int exponential) {
-        if(number == 0)
+        if(number == 0){
             return 0;
+        }
 
-        if(number == 1 || exponential == 0)
+        if(number == 1 || exponential == 0){
             return 1;
+        }
 
-        if(exponential == 1)
-            return  number;
+        if(exponential == 1) {
+            return number;
+        }
 
         return number * pow(number, exponential -1);
     }
